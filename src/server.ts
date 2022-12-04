17bab06a42c97378dbc42e4e6d8cbe4e637355cf
DevRafael-GL <rafaelgonsalveslima21@gmail.com>
2022-12-04 14:54:51 -0300

    Avatar de usuário

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,7 @@ app.use(router);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
+      next();
       return response.status(err.statusCode).json({
         message: err.message,
       });
